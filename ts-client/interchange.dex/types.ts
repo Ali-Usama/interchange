784680bf99a5efb8578d1965a46cbf6c40d1009d
Usama Ali <aliusama481@yahoo.com>
2023-02-23 15:42:57 +0500

    [ADD] * scaffold module dex --ibc --ordering unordered --dep bank

---
+++ ts-client/interchange.dex/types.ts
@@ -0,0 +1,11 @@
+import { DexPacketData } from "./types/interchange/dex/packet"
+import { NoData } from "./types/interchange/dex/packet"
+import { Params } from "./types/interchange/dex/params"
+
+
+export {     
+    DexPacketData,
+    NoData,
+    Params,
+    
+ }
